6c21763f0fbb9ad45c15f6651d3703c09e6caca4
Eryk1997 <33696058+Eryk1997@users.noreply.github.com>
2022-12-03 17:21:59 +0100

    format code

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,29 +1,15 @@
 Vue.createApp({
-    data() {
-        return {
-            descriptions: [],
-            description: '',
-        }
-    },
-
-    methods: {
-        addDescription() {
-            this.descriptions.push(this.description);
-            this.description = '';
-        }
+  data() {
+    return {
+      descriptions: [],
+      description: '',
     }
-}).mount('#app');
+  },
 
-// const inputEl = document.querySelector('input');
-// const buttonEl = document.querySelector('button');
-// const displayEl = document.querySelector('ul');
-
-// function addGoal() {
-//     const inputValue = inputEl.value;
-//     const listItemEl = document.createElement('li');
-//     listItemEl.textContent = inputValue;
-//     displayEl.appendChild(listItemEl);
-//     inputEl.value = '';
-// }
-
-// buttonEl.addEventListener('click', addGoal)
+  methods: {
+    addDescription() {
+      this.descriptions.push(this.description)
+      this.description = ''
+    },
+  },
+}).mount('#app')
