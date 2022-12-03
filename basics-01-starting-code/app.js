53a97b77a8c623cd07642ebe002716f81105dab3
Eryk1997 <33696058+Eryk1997@users.noreply.github.com>
2022-12-03 17:53:59 +0100

    define example property with string

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,3 +1,9 @@
-const app = Vue.createApp();
+const app = Vue.createApp({
+    data() {
+        return {
+            description: "Example user goal text",
+        };
+    }
+});
 
 app.mount('#user-goal');
