865c5cf884aff30cf7dc2a66f77e1926d1573226
Eryk1997 <33696058+Eryk1997@users.noreply.github.com>
2022-12-03 17:21:10 +0100

    create method addDescrption and use this method

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,10 +1,15 @@
-
-
 Vue.createApp({
     data() {
         return {
             descriptions: [],
             description: '',
+        }
+    },
+
+    methods: {
+        addDescription() {
+            this.descriptions.push(this.description);
+            this.description = '';
         }
     }
 }).mount('#app');
